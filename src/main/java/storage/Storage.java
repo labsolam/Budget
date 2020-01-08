d483c7ccb1381c22219f4ec4ca855604b4d00804
labsolam <labsolam@users.noreply.github.com>
2020-01-08 23:22:45 +0000

    Removed Java modules functionality
    Java modules was causing the Flyway exception issue. I decided to remove java modules in order to progress work on the app itself instead of getting hung up on minor details. I've kept the module-info file, renaming it to NOT_USED-module-info in the resources directory for when I take another look at introducing modules.

--- src/main/java/storage/Storage.java
+++ src/main/java/storage/Storage.java
@@ -1,11 +1,6 @@
 package storage;
 
 import org.flywaydb.core.Flyway;
-import org.flywaydb.core.internal.jdbc.DriverDataSource;
-import org.flywaydb.core.internal.scanner.classpath.ClassPathLocationScanner;
-
-import java.net.URLClassLoader;
-import java.sql.*;
 
 public class Storage
 {
@@ -16,36 +11,9 @@ public class Storage
 
 	}
 
-	public void loadDatabase() throws SQLException
+	public void loadDatabase()
 	{
 		Flyway flyway = Flyway.configure().dataSource(DATABASE_LOCATION,"","").load();
 		flyway.migrate();
-
-
-		ClassLoader.getSystemClassLoader().getResourceAsStream("db/migration/V1__Original_Structure.sql");
-		ClassLoader.getSystemClassLoader().getResource("db/migration/V1__Original_Structure.sql");
-		this.getClass().getClassLoader().getResourceAsStream("db/migration/V1__Original_Structure.sql");
-		this.getClass().getResource("/db/migration/V1__Original_Structure.sql");
-		this.getClass().getClassLoader().getResource("db/migration/V1__Original_Structure.sql");
-
-		System.out.println();
 	}
-
-
-//	public void getConnection()
-//	{
-//		try (
-//				Connection connection = DriverManager.getConnection("jdbc:h2:~/test");
-//				Statement statement = connection.createStatement();
-//				ResultSet resultSet = statement.executeQuery("select * from bob");
-//		)
-//		{
-//
-//		} catch (SQLException e)
-//		{
-//			System.out.println(e.getLocalizedMessage());
-//			System.out.println(e.getMessage());
-//			System.out.println("Caught SQL Excpetion");
-//		}
-//	}
 }
